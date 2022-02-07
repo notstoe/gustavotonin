891646566b1d6cbd30148cc1a0b9e3207b6264e9
notstoe <gstonin95@gmail.com>
2022-02-07 20:05:08 +0000

    Force new tab open with a tags

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -4,12 +4,16 @@ export default function Header() {
 	return (
 		<s.Header>
 			<s.NameWrapper>
-				<a href='https://github.com/notstoe'>Gustavo Tonin</a>
+				<a target='_blank' href='https://github.com/notstoe'>
+					Gustavo Tonin
+				</a>
 			</s.NameWrapper>
 
-			<a href='https://github.com/notstoe'>Projects</a>
+			<a target='_blank' href='https://github.com/notstoe'>
+				Projects
+			</a>
 			{/*
-			ABOUT section on version 2.0 of the portfolio
+			// TODO - ABOUT section on version 2.0 of the portfolio
 			<s.Separator>|</s.Separator>
 
 			<a href='https://github.com/notstoe'>About</a> */}
