fd907913f51319bb974db21d32b0c7e41534f512
notstoe <gstonin95@gmail.com>
2022-02-08 19:36:29 +0000

    SEO component optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+import SeoHead, { SeoProps } from 'Components/Head/SeoHead';
 import Intro from 'Components/Intro/Intro';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 const Home: NextPage = () => {
+	const seoInfo: SeoProps = {
+		title: 'Gustavo Tonin',
+		description: "Gustavo Tonin's Portfolio",
+		imageUrl:
+			'https://res.cloudinary.com/dowa8tjdi/image/upload/v1644348635/homepagePic_lbcazs.png',
+		websiteUrl: 'https://gustavotonin.vercel.app/',
+		// TODO - update to custom URL later
+	};
+
 	return (
 		<>
-			<Head>
-				<title>Gustavo Tonin</title>
-				<meta name='description' content="Gustavo Tonin's Portfolio" />
-				<meta name='og:description' content="Gustavo Tonin's Portfolio" />
-			</Head>
+			<SeoHead
+				title={seoInfo.title}
+				description={seoInfo.description}
+				imageUrl={seoInfo.imageUrl}
+				websiteUrl={seoInfo.websiteUrl}
+			/>
 
 			<Intro />
 		</>
